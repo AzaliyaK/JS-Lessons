7af0fae0d17f2e807b9c4544c5c59dc786e6f8e3
AzaliyaK <azaliya-frbi@rambler.ru>
2022-01-06 17:15:36 +0300

    create git rep

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 "use strict";
 
-
 // const user = 'Azaliya';
 // alert(`${user}, I'm glad to see you!`);
-
 
 const result = confirm("How are you?");
 console.log(result);
@@ -11,26 +9,25 @@ console.log(result);
 // alert('Как дела?');
 
 const answers = [];
-answers[0] = prompt('Как твое имя?', '');
-answers[1] = prompt('Сколько тебе лет?', '21');
-answers[2] = prompt('Ты студент?', 'да');
-if (answers[2] == 'да') {
-    answers[3] = prompt('В каком городе ты учишься?', '');
-    answers[4] = prompt('Какой институт?', '');
-    answers[5] = prompt('Какой курс?', '1');
+answers[0] = prompt("Как твое имя?", "");
+answers[1] = prompt("Сколько тебе лет?", "21");
+answers[2] = prompt("Ты студент?", "да");
+if (answers[2] == "да") {
+  answers[3] = prompt("В каком городе ты учишься?", "");
+  answers[4] = prompt("Какой институт?", "");
+  answers[5] = prompt("Какой курс?", "1");
 }
 console.log(answers);
 document.write(answers);
 
 const answer = prompt("Вам есть 18?", "21");
-console.log(typeof (answer));
+console.log(typeof answer);
 console.log(answer);
 
 // let number = 5;
 // const leftBorderWidth = 1;
 // number = 14.4;
 // console.log(number);
-
 
 // // console.log(someth);
 
@@ -45,3 +42,18 @@ console.log(answer);
 // obj.age = 22;
 // console.log(obj);
 // console.log(obj["name"]);
+
+//// …or create a new repository on the command line
+// echo "# JS-Lessons" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/AzaliyaK/JS-Lessons.git
+// git push -u origin main
+//// …or push an existing repository from the command line
+// git remote add origin https://github.com/AzaliyaK/JS-Lessons.git
+// git branch -M main
+// git push -u origin main
+//// …or import code from another repository
+// You can initialize this repository with code from a Subversion, Mercurial, or TFS project.
