978912265d0907f7f24f149163f188ab48088762
AzaliyaK <azaliya-frbi@rambler.ru>
2022-01-07 13:36:13 +0300

    l1_12

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
 "use strict";
 
-const user = "Азалия";
-alert(`${user}, у тебя все получится!`);
+// const user = "Азалия";
+// alert(`${user}, у тебя все получится!`);
+
+const numberOfBooks = +prompt("Сколько книг ты прочитал?", "");
+const personalMovieDB = {
+  count: numberOfBooks,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Как называется 1 прочитанная книга?", ""),
+  b = +prompt("На сколько (1...10) тебе понравилась?", ""),
+  c = prompt("Как называется 2 прочитанная книга?", ""),
+  d = +prompt("На сколько (1...10) тебе понравилась?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
